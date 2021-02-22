5f5ba1c628bb1c8f6007fd3a61521e14c2766d1c
raymond <raymondomon@yahoo.com>
2021-02-22 12:02:14 +0100

    added role statements

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,8 +1,11 @@
-import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-
+import 'source-map-support/register'
+import * as AWS from 'aws-sdk'
+import * as uuid from 'uuid'
+import { parseUserId } from '../../auth/utils'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+
+
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
