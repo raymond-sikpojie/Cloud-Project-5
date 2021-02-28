fb39afe051e72b112ba4b5d08176a5466c591e55
raymond <raymondomon@yahoo.com>
2021-02-28 17:24:10 +0100

    refactored code

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,21 +1,14 @@
 import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import * as AWS from 'aws-sdk'
-import 'source-map-support/register'
 
-const docClient = new AWS.DynamoDB.DocumentClient()
-
-const todosTable = process.env.TODOS_TABLE
+import { deleteTodo } from '../../businessLogic/todo'
+// import { createLogger } from '../../utils/logger'
+// import { getUserId } from '../utils'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    
-    const todoId = event.pathParameters.todoId
 
-    await docClient.delete({
-        TableName: todosTable,
-        Key: {
-          todoId
-        }
-    }).promise()
+    const todoId = event.pathParameters.todoId
+    
+    await deleteTodo(todoId)
 
     return {
         statusCode: 201,
@@ -25,4 +18,33 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
         body:'Todo item deleted'
     }
 }
+
+
+// import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+// import * as AWS from 'aws-sdk'
+// import 'source-map-support/register'
+
+// const docClient = new AWS.DynamoDB.DocumentClient()
+
+// const todosTable = process.env.TODOS_TABLE
+
+// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    
+//     const todoId = event.pathParameters.todoId
+
+//     await docClient.delete({
+//         TableName: todosTable,
+//         Key: {
+//           todoId
+//         }
+//     }).promise()
+
+//     return {
+//         statusCode: 201,
+//         headers: {
+//             'Access-Control-Allow-Origin': '*'
+//         },
+//         body:'Todo item deleted'
+//     }
+// }
 
--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,30 +1,16 @@
 import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import * as AWS from 'aws-sdk'
-import 'source-map-support/register'
+
+import { getTodos } from '../../businessLogic/todo'
+// import { createLogger } from '../../utils/logger'
 import { getUserId } from '../utils'
-
-const docClient = new AWS.DynamoDB.DocumentClient()
-
-const todosTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     
     console.log("EVENT:", event);
 
     const userId = getUserId(event)
-    
-    const result = await docClient.query({
-        TableName : todosTable,
-        IndexName: "UserIdIndex",
-        KeyConditionExpression: 'userId = :userId',
-        ExpressionAttributeValues: {
-            ':userId': userId
-        },
-  
-        ScanIndexForward: false
-    }).promise()
 
-    const items = result.Items
+    const items = await getTodos(userId)
 
     return {
         statusCode: 200,
@@ -36,4 +22,44 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
         })
     }
 }
+
+
+// import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+// import * as AWS from 'aws-sdk'
+// import 'source-map-support/register'
+// import { getUserId } from '../utils'
+
+// const docClient = new AWS.DynamoDB.DocumentClient()
+
+// const todosTable = process.env.TODOS_TABLE
+
+// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    
+//     console.log("EVENT:", event);
+
+//     const userId = getUserId(event)
+    
+//     const result = await docClient.query({
+//         TableName : todosTable,
+//         IndexName: "UserIdIndex",
+//         KeyConditionExpression: 'userId = :userId',
+//         ExpressionAttributeValues: {
+//             ':userId': userId
+//         },
+  
+//         ScanIndexForward: false
+//     }).promise()
+
+//     const items = result.Items
+
+//     return {
+//         statusCode: 200,
+//         headers: {
+//             'Access-Control-Allow-Origin': '*'
+//         },
+//         body: JSON.stringify({
+//             items
+//         })
+//     }
+// }
 
